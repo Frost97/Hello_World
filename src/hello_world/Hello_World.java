8bdd8816e5247bbfabc24fec5689166cd0dd69cf
LAGEVEEN.HENDRIK <LAGEVEEN.HENDRIK@berufskolleg-aachen.de>
2016-11-29 11:02:38 +0100

    first commit

---
+++ src/hello_world/Hello_World.java
@@ -0,0 +1,16 @@
+
+package hello_world;
+
+/**
+ *
+ * @author lageveen.hendrik
+ */
+public class Hello_World {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+    
+}
