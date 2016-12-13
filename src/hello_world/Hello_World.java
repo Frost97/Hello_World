58b5b78305ec329f152de117b82ff0daf45cee9f
Forst97 <lageveenhendrik@gmail.com>
2016-12-13 10:44:09 +0100

    Deutsche Übersetzung

--- src/hello_world/Hello_World.java
+++ src/hello_world/Hello_World.java
@@ -10,7 +10,7 @@ public class Hello_World {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
+        System.out.println("Hallo Welt");
     }
     
 }
